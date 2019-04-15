949918dd6e692ea5cb456260e7f5c326cd1d4fbc
fakki <17051907@hdu.edu.cn>
2019-04-15 22:09:20 +0800

    modified the filesys of lab4, finished my_format, my_startsys and my_exitsys functions.

--- lab4/filesys.c
+++ lab4/filesys.c
@@ -11,16 +11,6 @@
 #define ROOTBLOCKNUM 2
 #define MAXOPENFILE 10
 
-/*void *mem = (void *)malloc(SIZE);
-    my_format(mem);
-    block0 *block = (block0 *)malloc(sizeof(block0));
-    memcpy(block, mem, sizeof(block0));
-    printf("%s %d %d\n", block->info, block->root, block->startblock);
-    fcb *fcb1 = (fcb *)malloc(sizeof(fcb));
-    memcpy(fcb1, (void *)(mem + 5*BLOCKSIZE), sizeof(fcb));
-    printf("%s %s %s %d %d\n", fcb1->filename, fcb1->exname, fcb1->time, fcb1->length, fcb1->free);
-    free(block);*/
-
 typedef struct FCB{
     char filename[8];       //file name
     char exname[3];         //file extension
@@ -38,8 +28,11 @@ typedef struct FAT{
 typedef struct USEROPEN{
     fcb ffcb;
     //dynamic info of the file is below
-    char dir[80];
     int count;
+    char free;
+    int dirno;
+    int diroff;
+    char dir[80];
     char fcbstate;  //1: modified. 0: not.
     char topenfile; //
 
@@ -52,100 +45,12 @@ typedef struct BLOCK0{
 }block0;
 
 unsigned char *myvhead; //virtual disk start address
-useropen *openfilelist[MAXOPENFILE]; //open file list array
+useropen openfilelist[MAXOPENFILE]; //open file list array
 int curdir; //current directory disc character fd
 char currentdir[80]; //current directory name
 unsigned char *startp; //v d data area start position
 
-int exist(fcb *cursor, char *str, int fod)
-{
-    int onetime = 1;
-    int blocknum = cursor->first;
-    int flag = -1;
-    fat *fat1 = (fat *)(myvhead+BLOCKSIZE+(blocknum-1)*sizeof(fat));
-    do{
-        if(onetime < 1)
-        {
-            blocknum = fat1->id;
-            fat1 = (fat *)(myvhead+BLOCKSIZE+(blocknum-1)*sizeof(fat));
-            onetime--;
-        }
-        fcb *fcb1 = (fcb *)(startp+(blocknum-1)*BLOCKSIZE);
-        //root
-        if(blocknum == 1)
-            fcb1 = fcb1+1;
-        while(fcb1)
-        {
-            if(fcb1->attribute == fod && strcmp(fcb1->filename, str) == 0)
-            {
-                flag = 1;
-                return fcb1->first;
-            }
-            fcb1 = fcb1+1;
-        }
-    }
-    while(fat1->id != END);
-
-    return flag;
-}
-
-int addopenfl(fcb ffcb, char dir[80], int topenfile)
-{
-    int res;
-
-    useropen *uo = (useropen *)malloc(sizeof(useropen));
-    uo->ffcb = ffcb;
-    uo->count = -1;
-    strcpy(uo->dir, dir);
-    uo->fcbstate = 0;
-    uo->topenfile = topenfile;
-
-    for(res = 0; res < MAXOPENFILE; res++)
-    {
-        if(openfilelist[res] == NULL)
-        {
-            memcpy(openfilelist[res], uo, sizeof(useropen));
-            break;
-        }
-    }
-
-    if(res >= MAXOPENFILE)
-    {
-        printf("Can't open more files!\n");
-        exit(0);
-    }
-
-    return res;
-}
-
-int findfblock()
-{
-    int first, i;
-    fat *fat1 = (fat *)malloc(sizeof(fat)), *fat2 = (fat *)malloc(sizeof(fat));
-    fat1 = (fat *)(myvhead + BLOCKSIZE);
-    fat2 = (fat *)(myvhead + 3*BLOCKSIZE);
-    for(i = 0; i < 1000; i++, fat1++, fat2++)
-    {
-        if(fat1->id == 0)
-        {
-            first = fat1->id;
-            break;
-        }
-    }
-
-    if(i >= 1000)
-    {
-        printf("No more memory!\n");
-        exit(0);
-    }
-
-    fat1->id = END;
-    fat2->id = END;
-    free(fat1);
-    free(fat2);
-
-    return first;
-}
+FILE *file; // = fopen("/home/hondor/Desktop/filesys1.txt", "w");
 
 char *gettime()
 {
@@ -154,195 +59,102 @@ char *gettime()
     return ctime(&timep);
 }
 
-void my_format(void *mem)
+void my_format()
 {
-    //init BLOCK0 1 block
-    block0 *block0_start = (block0 *)mem;
-    block0 *block0_data = (block0 *)malloc(sizeof(block0));
-    block0_data->info[0] = 0;
-    block0_data->root = 6;
-    block0_data->startblock = (unsigned char *)(mem + 5*BLOCKSIZE);
-    memcpy(block0_start, block0_data, sizeof(block0));
-    free(block0_data);
+	printf("\nwaiting to initialized fs...\n");
+	int i;
+	myvhead = (unsigned char *)malloc(SIZE);
+	startp = myvhead + 5*BLOCKSIZE;
+	//initialize block0 here...
+	//...
+	unsigned char *magicnum = "10101010";
+	memcpy(myvhead, magicnum, 8);
 
-    //init FAT1
-    fat *fat1_start = (fat *)(mem + BLOCKSIZE);
-    fat *fat1_data = (fat *)malloc(sizeof(fat));
-    fat1_data->id = FREE;
-    for(int i = 0; i < 1000; i++, fat1_start++)
-        memcpy(fat1_start, fat1_data, sizeof(fat));
-    free(fat1_data);
+	unsigned char *fat1 = (unsigned char *)(myvhead + BLOCKSIZE), *fat2 = (unsigned char *)(myvhead + 3*BLOCKSIZE);
+	fat *fat_init = (fat *)malloc(1000*sizeof(fat));
+	fat *cursor = fat_init;
+	for(i = 0; i < 5; i++, cursor++)
+		cursor->id = END; //?
+	for(;i < 1000; i++, cursor++)
+		cursor->id = FREE;
 
-    //init FAT2
-    fat *fat2_start = (fat *)(mem + 3*BLOCKSIZE);
-    fat *fat2_data = (fat *)malloc(sizeof(fat));
-    fat2_data->id = FREE;
-    for(int i = 0; i < 1000; i++, fat2_start++)
-        memcpy(fat2_start, fat2_data, sizeof(fat));
-    free(fat2_data);
+	cursor = fat_init + 5;
+	cursor->id = END;
+	fcb *fcbroot = (fcb *)malloc(sizeof(fcb));
+	strcpy(fcbroot->filename, "root");
+	strcpy(fcbroot->exname, "dir");
+	strcpy(fcbroot->time, gettime());
+	fcbroot->first = 6;
+	fcbroot->attribute = 0;
+	fcbroot->length = 0;   //?
+	fcbroot->free = 1;
+	memcpy(startp, fcbroot, sizeof(fcb));
 
-    //init root directory
-    fcb *root_dir = (fcb *)(mem + 5*BLOCKSIZE);
-    fcb *root_dir_data = (fcb *)malloc(sizeof(fcb));
-    root_dir_data->attribute = 0; //directory file
-    strcpy(root_dir_data->filename, "root");
-    strcpy(root_dir_data->exname, "");
-    strcpy(root_dir_data->time, gettime());
-    root_dir_data->length = 0; //directory file
-    root_dir_data->free = 0;
-    //find disk block
-    root_dir_data->first = findfblock();
-    memcpy(root_dir, root_dir_data, sizeof(fcb));
-    free(root_dir_data);
+	//. and .. directory
+	fcb *onep = (fcb *)malloc(sizeof(fcb)), *twop = (fcb *)malloc(sizeof(fcb));
+	memcpy(onep, fcbroot, sizeof(fcb));
+	memcpy(twop, fcbroot, sizeof(fcb));
+	strcpy(onep->filename, ".");
+	strcpy(twop->filename, "..");
+	memcpy(startp+sizeof(fcb), onep, sizeof(fcb));
+	memcpy(startp+2*sizeof(fcb), twop, sizeof(fcb));
+	fcbroot->length += 2;
+
+	memcpy(fat1, fat_init, 1000*sizeof(fat));
+	memcpy(fat2, fat_init, 1000*sizeof(fat));
+
+	free(onep);
+	free(twop);
+	free(fcbroot);
+	free(fat_init);
+	printf("initialized successfully!\n");
 }
 
-void * startsys(FILE *file)
+void startsys()
 {
-    void *mem = (void *)malloc(SIZE);
-    fread(mem, SIZE, 1, file);
-    return mem;
+	int i;
+	file = fopen("/home/hondor/Desktop/filesys1.txt", "r");
+	char magicnum[8];
+	fread(magicnum, 8, 1, file);
+	fseek(file, 0, SEEK_SET);
+	if(strcmp(magicnum, "10101010") != 0)
+		my_format();
+	else 
+	{	
+		myvhead = (unsigned char *)malloc(SIZE);
+		fread(myvhead, SIZE, 1, file);
+	}
+	startp = myvhead + 5*BLOCKSIZE;
+	//initialize openfilelist
+	for(i = 0; i < MAXOPENFILE; i++)
+		openfilelist[i].topenfile = 0;
+	//open root dir
+	fcb *fcbroot = (fcb *)malloc(sizeof(fcb));
+	memcpy(fcbroot, startp, sizeof(fcb));
+	curdir = 0;
+	openfilelist[0].ffcb = *fcbroot;
+	openfilelist[0].count = 0; //?
+	openfilelist[0].dirno = openfilelist[0].diroff = -1; //?
+	openfilelist[0].topenfile = 1;
+	openfilelist[0].fcbstate = 0;
+	strcpy(openfilelist[0].dir, "/");
+
+	free(fcbroot);
+	fclose(file);
 }
 
-void my_exitsys(void *mem, FILE *file)
+void my_exitsys()
 {
-    fwrite(mem, SIZE, 1, file);
-}
-
-char **splitdir(char *dirname)
-{
-    char **res = (char **)malloc(10*sizeof(char *));
-    for(int i = 0; i < 10; i++)
-        res[i] = (char *)malloc(sizeof(char)*100);
-    int curr = 0;
-    char str[80];
-    strcpy(str, dirname+1);
-    char *token = strtok(str, "/");
-    while(token != NULL)
-    {
-        strcpy(res[curr++], token);
-        printf("%s\n", token);
-        token = strtok(NULL, "/");
-    }
-    return res;
-
-}
-
-void my_cd(char *dirname)
-{
-    //directory "/root" as head
-    void *cursor;
-    char **dir = splitdir(dirname);
-    bool flag = false;
-    if(strcmp(dir[0], "root") == 0)
-    {
-        //from root
-        int index = 1;
-        cursor = (void *)startp + sizeof(fcb);
-        while(dir[index])
-        {
-            int next_blocknum;
-            if(next_blocknum = exist(cursor, dir[index], 0) == -1)
-                break;
-            else {
-                index++;
-                cursor = (void *)startp + (next_blocknum-1)*BLOCKSIZE;
-            }
-        }
-        if(dir[index] == NULL)
-            flag = true;
-        else
-        {
-            printf("No such directory!\n");
-            return;
-        }
-        if(flag) printf("%s>>", dirname);
-    }
-    else
-    {
-        //from curr directory
-        int index = 1;
-        int blocknum = openfilelist[curdir]->ffcb.first;
-
-        cursor = (void *)startp + sizeof(fcb);
-        while(dir[index])
-        {
-            int next_blocknum;
-            if(next_blocknum = exist(cursor, dir[index], 0) == -1)
-                break;
-            else {
-                index++;
-                cursor = (void *)startp + (next_blocknum-1)*BLOCKSIZE;
-            }
-        }
-        if(dir[index] == NULL)
-            flag = true;
-        else
-        {
-            printf("No such directory!\n");
-            return;
-        }
-        if(flag) printf("%s>>", dirname);
-    }
-}
-
-void my_mkdir(char *dirname)
-{
-    //find current directory's block num
-    int blocknum = openfilelist[curdir]->ffcb.first;
-    fat *fat1 = (fat *)(myvhead + BLOCKSIZE);
-    fat1 += blocknum-1;
-    //not renamed
-    if(exist(&(openfilelist[curdir]->ffcb), dirname, 0) == -1)
-    {
-        _mkdir()
-    }
-}
-
-void my_ls()
-{
-    int onetime = 1;
-    int blocknum = openfilelist[curdir]->ffcb.first;
-    fat *fat1 = (fat *)(myvhead+BLOCKSIZE+(blocknum-1)*sizeof(fat));
-    do{
-        if(!onetime)
-            blocknum = fat1->id;
-        fcb *fcb1 = (fcb *)(startp+(blocknum-1)*BLOCKSIZE);
-        //root
-        if(blocknum == 1)
-            fcb1 = fcb1+1;
-        while(fcb1)
-        {
-            if(fcb1->attribute == 0)
-                printf("%s\n", fcb1->filename);
-            else printf("%s.%s\n", fcb1->filename, fcb1->exname);
-            fcb1++;
-        }
-    }
-    while(fat1->id != END);
+	file = fopen("/home/hondor/Desktop/filesys1.txt", "w");
+	fwrite(myvhead, SIZE, 1, file);
+	fclose(file);
 }
 
 int main()
 {
-    FILE *file;
-    file = fopen("/home/hondor/Desktop/filesys.txt", "r");
-    void *mem = startsys(file);
-    fclose(file);
+    startsys();
 
-    //root directory add to openfilelist
-    for(int i = 0; i < MAXOPENFILE; i++)
-        openfilelist[i] = NULL;
-
-    myvhead = (unsigned char *)mem;
-    startp = (unsigned char *)(mem + 5*BLOCKSIZE);
-    strcpy(currentdir, "/root");
-    fcb *root_fcb = (fcb *)malloc(sizeof(fcb));
-    memcpy(root_fcb, startp, sizeof(fcb));
-    curdir = addopenfl(*root_fcb, currentdir, 0);
-
-    file = fopen("/home/hondor/Desktop/filesys.txt", "w");
-    my_exitsys(mem, file);
-    fclose(file);
-
-    free(mem);
+    my_exitsys();
+    
     return 0;
 }
