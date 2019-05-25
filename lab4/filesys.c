1b404d9dd4489f75e0238db8c9dcf189acbe47e2
fakki <17051907@hdu.edu.cn>
2019-05-25 12:57:26 +0800

    add function of data file.

--- lab4/filesys.c
+++ lab4/filesys.c
@@ -56,6 +56,7 @@ fcb *getfcbposition(fcb *fcbcursor, int *dirno, int *diroff, char *filename);
 fcb *getfcb(int fd);
 char * makedirpath(char *dirname, char *filename);
 char *gettime();
+int freeblk(int blkno);
 int nextblkno(int blkno);
 int addblk(int blkno);
 void my_format();
@@ -69,6 +70,10 @@ int my_close(int fd);
 void my_cd(char *dirname);
 void my_ls();
 void my_rmdir(char *dirname);
+int my_create(char *filename);
+int my_read(int fd, int len);
+int my_write(int fd);
+int getfd(char *filename);
 
 int main()
 {
@@ -101,6 +106,28 @@ int main()
     	{
     		my_exitsys();
     	}
+    	else if(strcmp(order, "create") == 0)
+    	{
+    		scanf("%s", filename);
+    		my_create(filename);
+    	}
+    	else if(strcmp(order, "read") == 0)
+    	{
+    		int fd, len;
+    		scanf("%s %d", filename, &len);
+    		fd = my_open(filename);
+    		my_read(fd, len);
+    		my_close(fd);
+    	}
+    	else if(strcmp(order, "write") == 0)
+    	{
+    		int fd, len;
+    		scanf("%s", filename);
+    		fd = my_open(filename);
+    		my_write(fd);
+    		my_close(fd);
+
+    	}
     	else 
     	{
     		printf("command not found.\n");
@@ -112,16 +139,133 @@ int main()
     return 0;
 }
 
+int my_create(char *filename)
+{
+	int fcbslen = openfilelist[curdir].ffcb.length * sizeof(fcb);
+	char fcbs[fcbslen+1+sizeof(fcb)];
+	fcb *fcbcursor = (fcb *)fcbs + 3, *fcbobj = NULL;
+	do_read(curdir, fcbslen, fcbs);
+
+
+
+	for(int i = 3; i < openfilelist[curdir].ffcb.length;fcbcursor++)
+	{
+		if(fcbcursor->free == 0 && fcbobj == NULL) fcbobj = fcbcursor;
+		else 
+		{
+			if(strcmp(fcbcursor->filename, filename) == 0)
+			{
+				printf("file already exist.\n");
+				return 0;
+			}
+			i++;
+		}
+ 
+	}
+
+	//file is not created and this directory has no empty struct pcb
+	if(fcbobj == NULL)
+	{
+		fcbslen += sizeof(fcb);
+		fcbobj = fcbcursor;
+	}
+
+	if(fcbobj)
+	{
+		fcbobj->free = 1;
+		strcpy(fcbobj->filename, filename);
+		fcbobj->first = addblk(0);
+		fcbobj->length = 0;
+		fcbobj->attribute = 1;
+		strcpy(fcbobj->time, gettime());
+		//while((*filename) != '.') filename++;
+		//strcpy(fcbobj->exname, filename);
+	}
+
+	openfilelist[curdir].ffcb.length++;
+
+	if(do_write(curdir, fcbs, fcbslen, 2) == -1) printf("create failed.\n");
+
+}
+
+int my_write(int fd)
+{
+	if(fd >= MAXOPENFILE || fd < 0)
+	{
+		printf("file description invalid.\n");
+		return -1;
+	}
+
+	assert(openfilelist[fd].ffcb.attribute == 1);
+
+	int wstyle, blkno, nextblk, len;
+	char buf[BLOCKSIZE];
+
+	printf("please input your way to write:\n");
+	scanf("%d", &wstyle);
+
+	if(wstyle == 1)
+	{
+		blkno = nextblkno(openfilelist[fd].ffcb.first);
+		while(blkno != END)
+		{
+			nextblk = nextblkno(blkno);
+			freeblk(blkno);
+			blkno = nextblk;
+		}
+		openfilelist[fd].count = 0;
+		openfilelist[fd].ffcb.length = 0;
+	}
+	else if(wstyle == 3)
+	{
+		 openfilelist[fd].count = openfilelist[fd].ffcb.length;
+	}
+
+	getchar();
+
+	while(fgets(buf, BLOCKSIZE, stdin))
+	{
+		//printf("my_write line230:%d\n", strlen(buf));
+		int buflen = strlen(buf);
+		if(buflen == 1) break;
+		assert(do_write(fd, buf, buflen, wstyle) == buflen);
+		openfilelist[fd].ffcb.length += buflen;
+		openfilelist[fd].fcbstate = 1;
+	}
+	//while(scanf("%s"))
+
+}
+
+int my_read(int fd, int len)
+{
+	char text[len+1];
+	if(fd >= MAXOPENFILE || fd < 0)
+	{
+		printf("file description invalid.\n");
+		return -1;
+	}
+	text[len] = 0;
+	if(do_read(fd, len, text) == len)
+	{
+		printf("%s\n", text);
+		return len;
+	}
+	else return -1;
+
+	return 0;
+}
+
 fcb *getfcbposition(fcb *fcbcursor, int *dirno, int *diroff, char *filename)
 {
 	*dirno =  fcbcursor->first, *diroff = 1;
-	//printf("%d\n", fcbcursor->length);
+	int openfdlen = fcbcursor->length;
 	fcbcursor = (fcb *)(myvhead + BLOCKSIZE*(fcbcursor->first-1));
+	fcbcursor->length = openfdlen;
 	int plen = fcbcursor->length, i;
 	if(plen <= 3)
 	{
-		return NULL;
 		printf("no such file\n");
+		return NULL;
 	}
 
 	fcbcursor += 3;
@@ -143,7 +287,8 @@ fcb *getfcbposition(fcb *fcbcursor, int *dirno, int *diroff, char *filename)
 		}
 	}
 
-	return (fcb *)(myvhead + BLOCKSIZE*(fcbcursor->first-1));
+	if(fcbcursor->attribute == 0) return (fcb *)(myvhead + BLOCKSIZE*(fcbcursor->first-1));
+	else return fcbcursor;
 }
 
 fcb *getfcb(int fd)
@@ -153,7 +298,7 @@ fcb *getfcb(int fd)
 
 char * makedirpath(char *dirname, char *filename)
 {
-	char buf[30];
+	char *buf = (char *)malloc(80);
 	strcpy(buf, dirname);
 	strcat(buf, "/");
 	strcat(buf, filename);
@@ -296,6 +441,12 @@ void startsys()
 int do_read(int fd, int len, char *text)
 {
     //char *texttmp = text;
+    //printf("do_read line447:%d\n", openfilelist[fd].ffcb.length);
+    if((openfilelist[fd].ffcb.attribute == 1 && len > openfilelist[fd].ffcb.length) || (openfilelist[fd].ffcb.attribute == 0 && len > openfilelist[fd].ffcb.length*sizeof(fcb)))
+    {
+    	printf("the file is not such large.\n");
+    	return -1;
+    }
 	int read_byte = 0;
 	useropen file = openfilelist[fd];
 	int blocknum = file.ffcb.first;
@@ -558,6 +709,7 @@ int my_open(char *filename)
 	{
 		if(openfilelist[i].topenfile == 1 && strcmp(filename, openfilelist[i].ffcb.filename) == 0)
 		{
+			printf("file already opened.\n");
 			return i;
 		}
 	}
@@ -565,7 +717,11 @@ int my_open(char *filename)
 	int dirno, diroff;
 	fcb *fcbcursor = getfcbposition(&(openfilelist[curdir].ffcb), &dirno, &diroff, filename);
 
-	if(fcbcursor == NULL) return -1;
+	if(fcbcursor == NULL) 
+	{
+		printf("not found such file.\n");
+		return -1;
+	}
 
 	int fd = -1;
 	for(i =0; i < MAXOPENFILE; i++)
@@ -608,7 +764,20 @@ int my_close(int fd)
 	if(openfilelist[fd].fcbstate == 1)
 	{
 		openfilelist[fd].count = 0;
-		assert(do_write(fd, (char *)(&openfilelist[fd].ffcb), sizeof(fcb), 2) == sizeof(fcb));
+		if(openfilelist[fd].ffcb.attribute == 0) assert(do_write(fd, (char *)(&openfilelist[fd].ffcb), sizeof(fcb), 2) == sizeof(fcb));
+
+		else 
+		{
+			int  dirno = openfilelist[fd].dirno, diroff = openfilelist[fd].diroff;
+			fcb *fcbcursor = (fcb *)(myvhead + (dirno-1)*BLOCKSIZE);
+			while(nextblkno(dirno) != END)
+			{
+				dirno = nextblkno(dirno);
+				fcbcursor = (fcb *)(myvhead + (dirno-1)*BLOCKSIZE);
+			}
+			fcbcursor += (diroff+3-1);
+			memcpy(fcbcursor, &openfilelist[fd].ffcb, sizeof(fcb));
+		}
 	}
 
 	openfilelist[fd].topenfile = 0;
